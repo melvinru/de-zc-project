af171ae00fc3dcb94fd645f2251865ffaab1c419
melvinru <nikolaevdn@gmail.com>
2023-07-01 20:47:41 +0300

    Update .gitignoreAdd JetBrains IDE configuration folder and improve ETL processing
    Updated `.gitignore` to exclude JetBrains IDE configuration folder files for cleaner repository. Modified `etl_web_to_gcs.py` to fetch station data from an API and generate the dataset URL. Introduced error handling for cases when the provided station ID does not exist. The changes enhance the reusability and robustness of the ETL process.

--- flows/etl_web_to_gcs.py
+++ flows/etl_web_to_gcs.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+import requests
 
 
 @task(retries=3)
@@ -15,13 +16,13 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
     df["DATE"] = pd.to_datetime(df["DATE"])
-    df["ELEVATION"] = pd.to_numeric(df["ELEVATION"], downcast='integer')
-    df["LATITUDE"] = pd.to_numeric(df["LATITUDE"], downcast='float')
-    df["LONGITUDE"] = pd.to_numeric(df["LONGITUDE"], downcast='float')
-    df["TMAX"] = pd.to_numeric(df["TMAX"], downcast='float')
-    df["TMIN"] = pd.to_numeric(df["TMIN"], downcast='float')
-    df["TAVG"] = pd.to_numeric(df["TAVG"], downcast='float')
-    df["PRCP"] = pd.to_numeric(df["PRCP"], downcast='integer')
+    df["ELEVATION"] = pd.to_numeric(df["ELEVATION"], downcast="integer")
+    df["LATITUDE"] = pd.to_numeric(df["LATITUDE"], downcast="float")
+    df["LONGITUDE"] = pd.to_numeric(df["LONGITUDE"], downcast="float")
+    df["TMAX"] = pd.to_numeric(df["TMAX"], downcast="float")
+    df["TMIN"] = pd.to_numeric(df["TMIN"], downcast="float")
+    df["TAVG"] = pd.to_numeric(df["TAVG"], downcast="float")
+    df["PRCP"] = pd.to_numeric(df["PRCP"], downcast="integer")
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -44,16 +45,58 @@ def write_gcs(path: Path) -> None:
     return
 
 
-@flow()
-def etl_web_to_gcs() -> None:
-    """The main ETL function"""
-    station = "RSM00024688" #This is the coldest place in the world called Ojmjakon, there will be a search later for all the stations of interest.
-    dataset_file = f"{station}"
-    dataset_url = f"https://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/{station}.csv"
-    df = fetch(dataset_url)
+@task()
+def fetch_station_list() -> dict:
+    """Fetches and returns a dictionary of station details."""
+    url = "https://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/doc/ghcnd-stations.txt"
+    response = requests.get(url)
+    lines = response.text.split("\n")
+    stations = {}
+
+    for line in lines:
+        if line:
+            station_id = line[:11].strip()
+            latitude = line[12:20].strip()
+            longitude = line[21:30].strip()
+            elevation = line[31:37].strip()
+            stations[station_id] = {
+                "latitude": latitude,
+                "longitude": longitude,
+                "elevation": elevation,
+            }
+
+    return stations
+
+
+@task()
+def generate_dataset_url(station: str) -> str:
+    """Generates dataset URL"""
+    return f"https://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/{station}.csv"
+
+
+@task()
+def etl_process(df, dataset_file):
+    """Process the ETL steps."""
     df_clean = clean(df)
     path = write_local(df_clean, dataset_file)
     write_gcs(path)
+
+
+@flow()
+def etl_web_to_gcs(station: str = "RSM00024688") -> None:
+    """The main ETL function."""
+    stations = fetch_station_list()
+
+    if station not in stations:
+        print(
+            f"Could not find station '{station}'. Please check the station ID and try again."
+        )
+        return
+
+    dataset_file = station
+    dataset_url = generate_dataset_url(station)
+    df = fetch(dataset_url)
+    etl_process(df, dataset_file)
 
 
 if __name__ == "__main__":
